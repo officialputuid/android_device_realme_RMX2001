86c0f835687444c1905eef8d0351eaaa44e9c079
officialputuid <officialputuid@hack.id>
2021-08-21 13:55:48 +0800

    RMX2001: Adapt for Styx Project
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- styx_RMX2001.mk
+++ styx_RMX2001.mk
@@ -1,17 +1,7 @@
+##
+# Copyright (C) Android Open Source Project
 #
-# Copyright (C) 2020 Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
@@ -24,11 +14,11 @@ $(call inherit-product, device/realme/RMX2001/device.mk)
 # Call proprietary blob setup
 $(call inherit-product, vendor/realme/RMX2001/RMX2001-vendor.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Styx stuff
+$(call inherit-product, vendor/styx/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_RMX2001
+PRODUCT_NAME := styx_RMX2001
 PRODUCT_DEVICE := RMX2001
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme G90T Series
