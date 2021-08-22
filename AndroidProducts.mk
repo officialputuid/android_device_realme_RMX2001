2e5c1b8c79ab09d8099de4d86f8e2504ba61c847
officialputuid <officialputuid@hack.id>
2021-08-22 19:13:30 +0800

    RMX2001: Adapt for PalladiumOS
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,18 +1,13 @@
 #
-# Copyright (C) 2020 Android Open Source Project
+# Copyright (C) Android Open Source Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX2001.mk
+    $(LOCAL_DIR)/palladium_RMX2001.mk
+
+COMMON_LUNCH_CHOICES := \
+    palladium_RMX2001-user \
+    palladium_RMX2001-userdebug \
+    palladium_RMX2001-eng
---
+++ palladium_RMX2001.mk
@@ -0,0 +1,57 @@
+#
+# Copyright (C) Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/realme/RMX2001/device.mk)
+
+# Call proprietary blob setup
+$(call inherit-product, vendor/realme/RMX2001/RMX2001-vendor.mk)
+
+# Inherit some common Palladium stuff.
+$(call inherit-product, vendor/palladium/config/common_full_phone.mk)
+
+# Palladium stuff
+PALLADIUM_BUILDTYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Build with GApps
+TARGET_GAPPS_ARCH := arm64
+PALLADIUM_BUILD_VARIANT := GAPPS
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+
+# True - OnePlus Launcher as default launcher
+# Remove - Lawnchair as default launcher
+WITH_OPAPPS=true
+
+# Palladium props
+PRODUCT_PRODUCT_PROPERTIES += \
+        ro.palladiumdevice.maintainer=officialputuid \
+        ro.palladiumdevice.cpu=MediaTek Helio G90T \
+        ro.palladiumdevice.display=6.5 \
+        ro.palladiumdevice.displaytype=IPS LCD \
+        ro.palladiumdevice.battery=4300mAh \
+        ro.palladiumdevice.camera=64MP+8MP+2MP+2MP
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := palladium_RMX2001
+PRODUCT_DEVICE := RMX2001
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme G90T Series
+PRODUCT_MANUFACTURER := realme
+
+# Build info
+BUILD_FINGERPRINT := "realme/RMX2001/RMX2001L1:10/QP1A.190711.020/1594211000:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=RMX2001 \
+    PRODUCT_NAME=RMX2001 \
+    PRIVATE_BUILD_DESC="full_oppo6785-user 10 QP1A.190711.020 0b36b6c4f9b3bf3b release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
